32e4549ec43f381e6349e1ab6deff4555087a0c3
Akiff Manji <akiff.manji@gmail.com>
2021-07-13 11:42:17 -0700

    Adds header, footer and home screen
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,10 +1,27 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark flat>
-      <v-spacer></v-spacer>
-    </v-app-bar>
-    <v-main>
-      <router-view></router-view>
-    </v-main>
-  </v-app>
+  <div>
+    <v-app>
+      <Header />
+      <v-main>
+        <v-container>
+          <router-view></router-view>
+        </v-container>
+      </v-main>
+      <Footer />
+    </v-app>
+  </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Header from "./components/layout/header/Header.vue";
+import Footer from "./components/layout/footer/Footer.vue";
+
+@Component({
+  components: {
+    Header,
+    Footer,
+  },
+})
+export default class App extends Vue {}
+</script>
--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -1,3 +1,59 @@
 <template>
-  <h1>Home goes here</h1>
+  <div>
+    <v-row>
+      <v-col cols="12">
+        <div class="text-center">
+          <h1>Improve The Privacy And Security Of Your Information</h1>
+          <p>
+            Learn about Verifiable Credentials: a secure, privacy-focused way to
+            store and share your personal information using your smartphone.
+          </p>
+        </div>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col v-for="link in links" :key="link.path" cols="12" md="6">
+        <v-card outlined class="mb-4">
+          <v-card-title class="card-title">{{ link.title }}</v-card-title>
+          <v-card-text class="card-content">{{ link.description }}</v-card-text>
+          <v-card-actions>
+            <v-btn
+              :small="$vuetify.breakpoint.smAndDown"
+              block
+              class="text-capitalize"
+              :to="link.path"
+              >{{ link.enhancedLabel }}</v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { EnhancedNavLink, links } from "../components/layout/header/Header.vue";
+
+interface Data {
+  links: EnhancedNavLink[];
+}
+
+@Component
+export default class Home extends Vue {
+  data(): Data {
+    return {
+      links: links,
+    };
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.card-title {
+  word-break: break-word;
+}
+.card-content {
+  min-height: 5rem;
+}
+</style>
